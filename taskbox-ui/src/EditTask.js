28fe0428d90c0a9448b455d512205a90c067f07e
MrRobot0420 <faizanbaig0510@gmail.com>
2021-10-01 21:21:59 +0530

    initial commit

---
+++ taskbox-ui/src/EditTask.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React from "react";
+import TaskForm from "./TaskForm";
+import TasksContainer from "./TasksContainer";
+
+const EditTask = (props) => {
+    const { id, title, status, editItem, handleToggle } = props
+
+    const formSubmit = (task) => {
+        axios.put(`http://localhost:3033/api/tasks/${task.id}`, task)
+        .then((response) => {
+            const result = response.data
+            editItem(result)
+            handleToggle()
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+    }
+
+
+    return(
+        <div>
+            <h2> Edit Task </h2>
+            <TaskForm
+                id={id}
+                title={title}
+                status={status}
+                formSubmit={formSubmit}
+            />
+        </div>
+    )
+}
+
+export default EditTask
